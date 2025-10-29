52d64bd1944b4945e203f5626d9aa9a0d12137aa
gabrieltorquato <68511421+gabrieltorquato@users.noreply.github.com>
2025-10-29 05:39:10 -0300

    Add files via upload

---
+++ javascript/operadoresComparacao.js
@@ -0,0 +1,27 @@
+const idadeV = 36
+const minhaIdade = "36"
+
+// operador igualdade
+// const comparacao = idadeV == minhaIdade
+const comparacao = idadeV != minhaIdade
+
+// maior que
+// const comparacao = idade > minhaIdade
+
+// menor que
+// const comparacao = idade < minhaIdade
+
+console.log('São diferentes? ', comparacao)
+
+const idade = 36
+const idadeIrmao = 18
+
+const maiordeIdade = idade >= 18
+const irmaoMaiordeIdade = idadeIrmao >= 18
+
+console.log('Sou maior de idade? ', maiordeIdade)
+console.log('Meu irmão é maior de idade? ', irmaoMaiordeIdade)
+
+// comparacao estrita
+const comparacaoEstrita = minhaIdade === idadeV
+console.log('São estritamente iguais? ', comparacaoEstrita)
