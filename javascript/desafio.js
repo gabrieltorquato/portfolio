a1e0889c918a5c4a19e1bbd44ec7be319b3e6647
gabrieltorquato <68511421+gabrieltorquato@users.noreply.github.com>
2025-10-30 05:42:23 -0300

    Add files via upload

---
+++ javascript/desafio.js
@@ -0,0 +1,11 @@
+console.log('Boas vindas a calculadora de trocos!')
+
+let valorCompra = 17.60
+let valorPago = 20 
+
+console.log('Valor da compra: R$', valorCompra)
+console.log('Valor recebido: R$', valorPago)
+
+let troco = valorPago - valorCompra
+
+console.log('Troco a devolver: R$', troco.toFixed(2))
