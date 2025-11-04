2fb2838af45f6fdd233e78bcc05a76fbc0128990
gabrieltorquato <68511421+gabrieltorquato@users.noreply.github.com>
2025-11-04 05:25:27 -0300

    Add files via upload

---
+++ javascript/escolha.js
@@ -0,0 +1,30 @@
+// entre 9 e 10: excelente
+// entre 7 e 8: bom
+// entre 4 e 6: medio
+// entre 0 e 3: ruim
+
+const notaDoAluno = 4
+
+switch(notaDoAluno) {
+    case 10:
+    case 9:
+         console.log('Excelente')
+         break
+    case 8:
+    case 7:
+         console.log('Bom')
+         break
+    case 6:
+    case 5:
+    case 4:
+         console.log('Mediano')
+         break
+    case 3:
+    case 2:
+    case 1:
+         console.log('Ruim')
+         break
+    default:
+        console.log('Valor inválido')
+        break
+}
