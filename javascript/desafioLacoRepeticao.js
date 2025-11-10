41fe1b7090f61a314d1c8d9baf00f3ab5ae09c13
gabrieltorquato <68511421+gabrieltorquato@users.noreply.github.com>
2025-11-10 05:37:24 -0300

    Add files via upload

---
+++ javascript/desafioLacoRepeticao.js
@@ -0,0 +1,16 @@
+// contar quantos numeros pares e quantos numeros impares nós temos entre 0 e 100
+
+let totalNumerosPares = 0
+let totalNumerosImpares = 0
+
+// laço de repetição
+
+for (let contador = 0; contador <= 100; contador++) {
+    if (contador % 2 == 0) {
+        totalNumerosPares++
+    } else {
+        totalNumerosImpares++
+    }
+}
+console.log('Total de número pares:', totalNumerosPares)
+console.log('Total de número ímpares:', totalNumerosImpares)
