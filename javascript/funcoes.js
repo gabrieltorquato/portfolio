2ebaf7e298dac22c37185648ced7bdfe051766a4
gabrieltorquato <68511421+gabrieltorquato@users.noreply.github.com>
2025-11-13 05:45:04 -0300

    Add files via upload

--- javascript/funcoes.js
+++ javascript/funcoes.js
@@ -1,14 +1,26 @@
-function saudacao(nome) { // nome é um parametro da função saudação
-    console.log('Olá,', nome)
-}
+//function saudacao(nome) { // nome é um parametro da função saudação
+// console.log('Olá,', nome)
+//}
+
+//const saudacao = (nome) => {
+//    console.log('Vida longa e prospera,',nome)
+//}
+
+const saudacao = nome => console.log('Olá,', nome)
 
 saudacao('Gabriel') // Gabriel é o argumento
 saudacao('Jéssica')
 saudacao('Nathalia')
 
-function calcularDobroDeUm(numero) {
-    return numero * 2
-}
+//function calcularDobroDeUm(numero) {
+//    return numero * 2
+//}
+
+//const calcularDobroDeUm = (numero) => {
+//    return numero * 2
+//}
+
+const calcularDobroDeUm = numero => numero * 2
 
 const numeroDobrado = calcularDobroDeUm(4)
 console.log('O dobro de 4 é:', numeroDobrado)
